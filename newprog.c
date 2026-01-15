601dfcf5e83aa1ddde5ec7cf21130afc8ad4d215
zaigiaz <shayne.tman@gmail.com>
2026-01-14 20:30:09 -0600

    Delete Extras, Finished Struct Functionality

--- newprog.c
+++ newprog.c
@@ -4,11 +4,17 @@
 
 #define FIELD_SEP ','
 #define NEWLINE '\n'
+#define BUF_SIZE 256
+
+// TODO: Add Data into Array of Structs
+// TODO: Add Helper Functions to convert to int or float if needed
+// TODO: add some Asserts and test cases to ensure code reliability
 
 struct fields {
-  char *name;
-  char *age;
-  char *date;
+  char name[BUF_SIZE];
+  char age[BUF_SIZE];
+  char date[BUF_SIZE];
+  /* char EXAMPLE_FIELD[BUF_SIZE]; */
 };
 
 int main(void) {
@@ -28,11 +34,12 @@ int main(void) {
 	}
     }
 
-
-  printf("Lines: %d\n", lines);
+  /* printf("Lines: %d\n", lines); */
+  
+  // array of fields that hold all data for CSV
   struct fields list[lines];
-
-  char buffer[256];
+  
+  char buffer[BUF_SIZE];
   buffer[0] = '\0';
   size_t len=0;
 
@@ -40,52 +47,59 @@ int main(void) {
   fseek(file, 0, SEEK_SET);
 
   char ch;
-  int field_count=0;
+  int list_count=0, count=0;
 
   while((ch = fgetc(file)) != EOF) {    
 
     if(ch == FIELD_SEP) {
+      /* printf("\nhere is a field: %s", buffer); */
 
       // null terminating string must have terminator
+      count += 1;
       buffer[len++] = '\0';
-      printf("\nhere is a field: %s", buffer);
+
+      switch(count) {
+      case 1:
+	/* logic here to strcpy buffer into struct field */
+	strcpy(list[list_count].name, buffer);
+	break;
+      case 2:
+	strcpy(list[list_count].age, buffer);
+	break;      
+      }
+      
       buffer[0] = '\0';
       len = 0;
-
-      /* switch(field_count) { */
-      /* case 1: */
-      /* 	lines[counter].name = build; */
-      /* case 2: */
-      /* 	lines[counter].age = build; */
-      /* case 3: */
-      /* 	lines[counter].date = build; */
-      /* } */
-
       continue;
     }
 
     if(ch == NEWLINE) {
-      /* counter += 1; */
+
+      // only increment list 
+      count = 0;
+
+      /* printf("\nhere is a field: %s", buffer); */
 
       buffer[len++] = '\0';
-      printf("\nhere is a field: %s", buffer);
+      strcpy(list[list_count].date, buffer);
       buffer[0] = '\0';
+
+      // reset buffer
       len = 0;
 
+      list_count += 1;
       continue;
      }
 
+
+
     // build the string here
-    
     buffer[len++] = ch;
-    /* printf("%c", ch); */
 
   }
 
+  printf("Here is the first field: %s", list[3].age);
+
   int close = fclose(file);
-  if(close == EOF) { printf("error at closing"); exit(1); }
-
-  printf("\n\nhello from end of function");
+  if(close == EOF) { printf("error at closing"); exit(1); }  
 }
-
-
