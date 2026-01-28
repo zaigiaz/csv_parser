dd3f2efdddd8b342ece0701a5ada865b86fc5a55
zaigiaz <shayne.tman@gmail.com>
2026-01-28 10:06:38 -0600

    added convert function

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+/* Program created by zaigiaz - 2026  */
+/* edit the fields, and convert function to tailor towards your csv schema */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,26 +10,44 @@
 #define NEWLINE '\n'
 #define BUF_SIZE 256
 
-// TODO: use atoi() and atof() to convert strings to floats and integers
 // TODO: add functionality to read first line of my schema
+// TODO: and add automatic type_checking maybe instead of using schema
 
 struct fields {
   char name[BUF_SIZE];
   int age;
-  char date[BUF_SIZE];
+  float number;
   /* char EXAMPLE_FIELD[BUF_SIZE]; */
 };
 
+struct fields convert(struct fields item, int char_point, char buffer[BUF_SIZE]) {
+  
+  switch(char_point) {
+  case 1:
+    strcpy(item.name, buffer);
+    break;
+  case 2:
+    char *bptr = buffer;
+    item.age = atoi(bptr);
+    break;
+  case 3:
+    char *fptr = buffer;
+    item.number = atof(fptr);
+    break;
+  }
+
+  return item;
+}
 
 // print all the rows in CSV
-void print_file(struct fields list[BUF_SIZE], int lines) {  
+void print_file(struct fields list[], int lines) {  
   printf("\nPrinting out All Fields | Debug\n\n");
 
   for(int i=0; i<lines; i++) {
     printf("-------------\n");
     printf("%s\n", list[i].name);
     printf("%d\n", list[i].age);
-    printf("%s\n", list[i].date);
+    printf("%f\n", list[i].number);
   }  
 }
 
@@ -59,6 +80,7 @@ int getNumberItems(FILE *file) {
   int lines=0;
 
   lines = getNumberItems(file);
+  fseek(file, 0, SEEK_SET);
   
   struct fields list[lines];
 
@@ -66,27 +88,24 @@ int getNumberItems(FILE *file) {
   buffer[0] = '\0';
   size_t len=0;
 
-  fseek(file, 0, SEEK_SET);
 
   char ch;
   int row_count=0, char_point=0;
 
   while((ch = fgetc(file)) != EOF) {    
 
+    // parse header right here
+    if(row_count == 0) {
+
+    }
+
+
     if(ch == FIELD_SEP) {
       char_point += 1;
       buffer[len++] = '\0';
-      // convert function here 
 
-      switch(char_point) {
-      case 1:
-	strcpy(list[row_count].name, buffer);
-	break;
-      case 2:
-	char *bptr = buffer;
-	list[row_count].age = atoi(bptr);
-	break;      
-      }
+      // convert function here 
+      list[row_count] = convert(list[row_count], char_point, buffer);
       
       buffer[0] = '\0';
       len = 0;
@@ -95,14 +114,16 @@ int getNumberItems(FILE *file) {
     }
 
     if(ch == NEWLINE) {
-      char_point = 0;
+      char_point += 1;
+      buffer[len++] = '\0';
 
       // have convert function here
-      buffer[len++] = '\0';
-      strcpy(list[row_count].date, buffer);
+      list[row_count] = convert(list[row_count], char_point, buffer);
+
       buffer[0] = '\0';
       
-      len = 0;
+      char_point = 0;
+      len        = 0;
       row_count += 1;
       continue;
      }
@@ -110,7 +131,7 @@ int getNumberItems(FILE *file) {
     buffer[len++] = ch;
   }
 
-  print_file(list, lines); 
+  print_file(list, lines);
   int close = fclose(file);
   if(close == EOF) { perror("error at closing"); }
 }
